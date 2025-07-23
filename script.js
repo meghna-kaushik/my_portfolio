28e4a4a3e66788430f887e825b050e91e2aa3503
Meghna Kaushik <meghnakaushik@admins-MacBook-Air.local>
2025-07-23 12:34:17 +0530

    Force rebuild for Github pages

--- script.js
+++ script.js
@@ -85,3 +85,19 @@ const navLinks = document.getElementById("nav-links");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+
+// Scroll to top button
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
